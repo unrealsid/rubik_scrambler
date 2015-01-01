61ee0772a662ae51f4ca8a2dd5a7a7977076eaf5
Steven Elliott <selliott512@users.noreply.github.com>
2015-01-01 14:00:36 -0600

    Added special non-movement keys.

--- Libraries/EventListeners.js
+++ Libraries/EventListeners.js
@@ -20,9 +20,11 @@ function onWindowResize(event)
 		
 function onDocumentKeyDown(event)
 {
-	var args = eventToRotation[String.fromCharCode(event.keyCode)];
+	var eventChar = String.fromCharCode(event.keyCode)
+	var args = eventToRotation[eventChar];
 	if (args)
 	{
+		// Regular movement
 		args = args.slice(); // Copy so the original is not changed.
 		args[1] += event.shiftKey ? "P" : ""; // Append P for prime.
 		// Convert the -1, 0, 1 placement along the axes to limits
@@ -30,5 +32,29 @@ function onDocumentKeyDown(event)
 		args[2] = cubieDist * args[2] - 1; // Lower limit, so 1 before.
 		args[3] = cubieDist * args[3] + 1; // Lower limit, so 1 after.
 		rotateFace.apply(this, args);
+	}
+	else
+	{
+		// Special keys
+		switch (eventChar)
+		{
+			// A lot of good letters were already taken.
+			case "A": // (A)nimation toggle
+				var animCB = document.getElementById("skipAnimation");
+				animCB.checked = !animCB.checked;
+				break;
+			case "N": // (N)ew cube (scramble)
+				displayScramble();
+				break;
+			case "O": // (O)rientation display toggle.
+				var orientCB = document.getElementById("dispOrientationLabels");
+				orientCB.checked = !orientCB.checked;
+				animate();
+				break;
+			case "Q": // (Q)uit the current cube
+				// TODO: Find a better way of doing this.
+				location.reload();
+				break;
+		}
 	}
 }
